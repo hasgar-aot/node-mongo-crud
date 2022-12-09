2b47083f29d882140c296127dd9e79c666643d1e
Hasgar D <hasgar.d@aot-technologies.com>
2022-12-09 15:08:37 +0530

    Initial commit

---
+++ models/Blog.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const blogSchema = new Schema(
+  {
+    title: String,
+    body: String,
+    image: String,
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { versionKey: false }
+);
+
+module.exports = mongoose.model("Blog", blogSchema);
